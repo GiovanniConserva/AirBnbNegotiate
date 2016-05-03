6d47c1edafa239caea695f87126fa4d8db4a8d33
GiovanniConserva <giovannibconserva@gmail.com>
2016-05-03 15:07:44 +0200

    configured for deploy

--- Website/AirbnbNegotiate/AirbnbNegotiate/settings.py
+++ Website/AirbnbNegotiate/AirbnbNegotiate/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'AirbnbNegotiate.urls'
@@ -82,6 +84,11 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -120,6 +127,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+#STATIC_URL = '/static/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 USE_L10N=False
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
---
+++ Website/AirbnbNegotiate/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\git\8\website\airbnbnegotiate\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
